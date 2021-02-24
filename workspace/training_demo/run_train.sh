12e637fb92d6bc8c197d7c2ee1c30024af2d957a
Victor H <46825640+vjhansen@users.noreply.github.com>
2021-02-24 16:38:08 +0100

    Update run_train.sh

--- workspace/training_demo/run_train.sh
+++ workspace/training_demo/run_train.sh
@@ -1,1 +1,1 @@
-python model_main_tf2.py --model_dir=models/efficientdet_d0 --pipeline_config_path=models/efficientdet_d0/pipeline.config  --alsologtostderr
+python3 model_main_tf2.py --model_dir=models/efficientdet_d0 --pipeline_config_path=models/efficientdet_d0/pipeline.config  --alsologtostderr
