b3b03204be55a5c1d7896912be847f9dbeeb63b0
victor <0vjhansen@gmail.com>
2021-02-26 08:51:20 -0500

    up

--- workspace/training_demo/run_train.sh
+++ workspace/training_demo/run_train.sh
@@ -1,3 +1,3 @@
 MODEL=$1
 
-python3 model_main_tf2.py --model_dir=models/$MODEL --pipeline_config_path=models/$MODEL/pipeline.config  --alsologtostderr --eval_on_train_data=true
+python3 model_main_tf2.py --model_dir=models/$MODEL --sample_1_of_n_eval_examples=10 --pipeline_config_path=models/$MODEL/pipeline.config  --alsologtostderr
