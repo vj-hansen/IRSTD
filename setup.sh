a23603a7f811348981fc6e4770413cf4438340cc
vjhansen <0vjhansen@gmail.com>
2021-03-05 13:19:07 +0100

    upload

--- setup.sh
+++ setup.sh
@@ -2,5 +2,4 @@ sudo apt-get update && sudo apt-get upgrade && \
     sudo apt install python3-pip && \
     sudo apt-get install python-tk protobuf-compiler python-lxml python-pil && \
     pip3 install -r requirements.txt && \
-    pip3 install --user --upgrade tensorflow tensorflow-gpu && \
-    git clone https://github.com/tensorflow/models.git
+    pip3 install --user --upgrade tensorflow tensorflow-gpu
