2e215662754728552e349f77c34386a63a27a7cb
Victor H <46825640+vjhansen@users.noreply.github.com>
2021-03-21 20:50:33 +0100

    Update run_train.sh

--- workspace/run_train.sh
+++ workspace/run_train.sh
@@ -1,4 +1,4 @@
 # update: 21.03.21, VJH
 MODEL=$1
 
-python3 model_main_tf2.py --model_dir=/home/paperspace/$MODEL --eval_on_train_data=True --sample_1_of_n_eval_examples=10 --pipeline_config_path=/home/paperspace/$MODEL/pipeline.config
+python3 model_main_tf2.py --model_dir=/home/paperspace/$MODEL --sample_1_of_n_eval_examples=10 --pipeline_config_path=/home/paperspace/$MODEL/pipeline.config
