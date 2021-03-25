5e22f91495ac4026f3e31f84c3ac575a66c9c6be
vjhansen <0vjhansen@gmail.com>
2021-03-25 10:35:11 +0100

    a

--- workspace/eval.sh
+++ workspace/eval.sh
@@ -1,4 +1,6 @@
-# update: 21.03.21, VJH
+# update: 24.03.21, VJH
 MODEL=$1
+
+# add --run_once=True
 
 python3 model_main_tf2.py --model_dir=/home/paperspace/$MODEL --pipeline_config_path=/home/paperspace/$MODEL/pipeline.config --checkpoint_dir=/home/paperspace/$MODEL
