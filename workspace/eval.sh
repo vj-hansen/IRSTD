280cc405e3af8e2548bda38249956a93f847e33b
Victor <0vjhansen@gmail.com>
2021-03-28 11:19:25 -0400

    a

--- workspace/eval.sh
+++ workspace/eval.sh
@@ -1,6 +1,4 @@
 # update: 24.03.21, VJH
 MODEL=$1
 
-# add --run_once=True
-
-python3 model_main_tf2.py --model_dir=/home/paperspace/$MODEL --pipeline_config_path=/home/paperspace/$MODEL/pipeline.config --checkpoint_dir=/home/paperspace/$MODEL
+python3 model_main_tf2.py --model_dir=/home/paperspace/$MODEL --pipeline_config_path=/home/paperspace/$MODEL/pipeline.config --checkpoint_dir=/home/paperspace/$MODEL --run_once=True
