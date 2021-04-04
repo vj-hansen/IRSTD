acd202d15de941dd1c1a51e00b4e5b360b7df4d8
Victor <0vjhansen@gmail.com>
2021-04-04 09:11:29 -0400

    a

--- workspace/run_train.sh
+++ workspace/run_train.sh
@@ -1,4 +1,7 @@
-# update: 21.03.21, VJH
+# update: 02.04.21, VJH
 MODEL=$1
+
+rm -rf /home/paperspace/$MODEL/c* && \
+rm -rf /home/paperspace/$MODEL/train/ && \
 
 python3 model_main_tf2.py --model_dir=/home/paperspace/$MODEL --sample_1_of_n_eval_examples=10 --pipeline_config_path=/home/paperspace/$MODEL/pipeline.config
