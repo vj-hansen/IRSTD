282c10acbcc339f5d736a6d0e540b1f417aea02e
Victor Hansen <vjhansen7@gmail.com>
2022-11-01 22:27:06 +0100

    chore: updates
    remove data-driven method (see legacy branch)
    
    Add argparsing
    Clean gitignore
    Update README
    Use protected methods
    Remove model_driven_method dir
    Add logging

--- setup_linux.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update && sudo apt upgrade && \
-    sudo apt install python3-pip && \
-    sudo apt install python-tk protobuf-compiler python-lxml python-pil && \
-    pip3 install -r requirements.txt && \
-    pip3 install --user --upgrade tensorflow tensorflow-gpu
--- workspace/run_train.sh
+++
@@ -1,6 +0,0 @@
-MODEL=$1
-
-rm -rf /home/paperspace/$MODEL/c* && \
-rm -rf /home/paperspace/$MODEL/train/ && \
-
-python3 model_main_tf2.py --model_dir=/home/paperspace/$MODEL --sample_1_of_n_eval_examples=10 --pipeline_config_path=/home/paperspace/$MODEL/pipeline.config
