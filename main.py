d6600549c853b7de57eaed4c3d97210f87836287
Victor Hansen <victor.hansen@remarkable.no>
2023-09-15 16:53:17 +0200

    chore: updates
    Add argparsing
    Clean gitignore
    Update README
    Use protected methods
    Remove model_driven_method dir
    Add logging

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ Dec. 2013, doi: 10.1109/TIP.2013.2281420.
 
 """
 
+import argparse
 import os
 import time
+from pathlib import Path
 
 import cv2
 import numpy as np
+from loguru import logger
 from matplotlib import pyplot
 from PIL import Image
 
@@ -22,7 +25,7 @@ from pcp import pcp_func
 
 
 class MainClass:
-    def __init__(self):
+    def __init__(self, method: str, input_image_dir: str):
         self.total_time = 0
         self.true_pos = 0
         self.false_pos = 0
@@ -32,22 +35,22 @@ class MainClass:
         self.total_detc = []
         self.max_it_param = 500
         self.tol_param = 1e-2
-        self.method_param = "ialm"  # or apg
+        self.method_param = method
         self.thresh_param = 150
         self.slidewin_step_size = 20
         self.slidewin_patch_size = 80
         self.delta = 4
-        cwd = os.getcwd()
-        self.test_dir = f"{cwd}/model_driven_method/test_imgs/"
+        self.test_dir = f"{Path.cwd()}/{input_image_dir}/"
         self.img_dir = os.listdir(self.test_dir)
-        self.save_dir = "model_driven_method/detection_pics/"
+        self.save_dir = "detection_pics/"
 
     def set_dirs(self):
-        if not os.path.exists(self.save_dir):
-            os.makedirs(self.save_dir)
+        if not Path(self.save_dir).exists():
+            Path(self.save_dir).mkdir(parents=True)
 
     def get_files(self):
         filelist = [file for file in self.img_dir if file.endswith(".png")]
+        logger.debug(filelist)
         return filelist
 
     def run_pcp(self, img, im_shape):
@@ -77,7 +80,7 @@ class MainClass:
     def get_time(self, start, end):
         round_time = end - start
         self.total_time = self.total_time + round_time
-        print("Total time: %.2f s" % round_time)
+        logger.debug("Total time: %.2f s" % round_time)
 
     def iter_image(self, iter_, read_xml_file, circ_img_rgb, gtcx_arr, gtcy_arr):
         ymin_xml = read_xml_file[iter_][2]
@@ -108,9 +111,11 @@ class MainClass:
         return im_status, p_order, gt_order
 
     def pts_prox(self, gt_bbx, pred_bbx, pcx_pos, gt_objects_in_img, status_img) -> str:
-        # return true if objects are within proximity
-        pts_close = pts_near(gt_bbx, pred_bbx, rad=5)
+        pts_close = pts_near(
+            gt_bbx, pred_bbx, rad=5
+        )  # return true if objects are within proximity
         status_img.append(pts_close)
+        im_status = ""
         if pts_close and gt_objects_in_img == len(pcx_pos):
             self.true_pos += 1
             if sum(status_img) == gt_objects_in_img:
@@ -141,6 +146,7 @@ class MainClass:
 
     def run(self) -> None:
         filelist = self.get_files()
+        im_status = ""
         for _, file in enumerate(filelist):
             gt_objects_in_img, read_xml_file = self.check_files(file)
             img = pyplot.imread("img.jpg")
@@ -154,7 +160,7 @@ class MainClass:
 
             self.img_filename.append(file.split(".")[0])
             pyplot.imsave("t_img.jpg", T.reshape(im_shape), cmap="gray")
-            print(f"{str(gt_objects_in_img)} object(s) in {file}")
+            logger.info(f"{str(gt_objects_in_img)} object(s) in {file}")
 
             circ_img_rgb, pcx_pos, pcy_pos = get_target_loc(
                 "t_img.jpg", thresh=self.thresh_param, delta=self.delta
@@ -194,15 +200,39 @@ class MainClass:
                 self.false_neg += 1
                 im_status = "FN_"
 
+            image_write = os.path.join(
+                self.save_dir,
+                f"{im_status}{self.method_param}_{self.tol_param}_{self.max_it_param}_{self.thresh_param}_{file.split('.')[0]}_target.jpg",
+            )
+            logger.debug(image_write)
             cv2.imwrite(
-                f"{self.save_dir}{im_status}_{self.method_param}_{self.tol_param}_{self.max_it_param}_{self.thresh_param}_{file.split('.')[0]}_target.jpg",
+                image_write,
                 circ_img_rgb,
             )
-            print_results(filelist)
+            self.print_results(filelist)
 
-        def print_results(self, filelist) -> None:
-            avg_time = self.total_time / (len(filelist))
-            print("Avg. time per img.: %.2f s" % avg_time)
-            print("TP: ", self.true_pos)
-            print("FP: ", self.false_pos)
-            print("FN: ", self.false_neg)
+    def print_results(self, filelist) -> None:
+        avg_time = self.total_time / (len(filelist))
+        logger.info("Avg. time per img.: %.2f s" % avg_time)
+        logger.info(f"TP: {self.true_pos}")
+        logger.info(f"FP: {self.false_pos}")
+        logger.info(f"FN: {self.false_neg}")
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--method", type=str, help="Model driven method", choices=["ialm", "apg"]
+    )
+    parser.add_argument("--image-dir", type=str, help="Image dir name")
+    args = parser.parse_args()
+
+    assert args.method, "No method provided... Aborting!"
+    assert args.image_dir, "No image dir provided... Aborting!"
+
+    MainClass(method=args.method, input_image_dir=args.image_dir).run()
+
+
+if __name__ == "__main__":
+    main()
